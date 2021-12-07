517bffb30479f9e72c4496e8cab2069e59342e65
Djoulzy <29226401+Djoulzy@users.noreply.github.com>
2021-12-08 00:08:43 +0100

    anim

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"image/color"
 	"time"
 
+	"GoLedMatrix/clog"
 	"GoLedMatrix/confload"
 	"GoLedMatrix/rgbmatrix"
 
@@ -28,62 +29,12 @@ func main() {
 	m, err := rgbmatrix.NewRGBLedMatrix(&config.HardwareConfig, &config.RuntimeOptions)
 	fatal(err)
 
-	tk := rgbmatrix.NewToolKit(m)
-	defer tk.Close()
-
-	tk.PlayAnimation(NewAnimation(image.Point{128, 64}))
-}
-
-func init() {
-	flag.Parse()
+	BouncingBall()
 }
 
 func fatal(err error) {
 	if err != nil {
+		clog.Fatal("GoLedMatrix", "main", err)
 		panic(err)
-	}
-}
-
-type Animation struct {
-	ctx      *gg.Context
-	position image.Point
-	dir      image.Point
-	stroke   int
-}
-
-func NewAnimation(sz image.Point) *Animation {
-	return &Animation{
-		ctx:    gg.NewContext(sz.X, sz.Y),
-		dir:    image.Point{1, 1},
-		stroke: 5,
-	}
-}
-
-func (a *Animation) Next() (image.Image, <-chan time.Time, error) {
-	defer a.updatePosition()
-
-	a.ctx.SetColor(color.Black)
-	a.ctx.Clear()
-
-	a.ctx.DrawCircle(float64(a.position.X), float64(a.position.Y), float64(a.stroke))
-	a.ctx.SetColor(color.RGBA{255, 0, 0, 255})
-	a.ctx.Fill()
-	return a.ctx.Image(), time.After(time.Millisecond * 50), nil
-}
-
-func (a *Animation) updatePosition() {
-	a.position.X += 1 * a.dir.X
-	a.position.Y += 1 * a.dir.Y
-
-	if a.position.Y+a.stroke > a.ctx.Height() {
-		a.dir.Y = -1
-	} else if a.position.Y-a.stroke < 0 {
-		a.dir.Y = 1
-	}
-
-	if a.position.X+a.stroke > a.ctx.Width() {
-		a.dir.X = -1
-	} else if a.position.X-a.stroke < 0 {
-		a.dir.X = 1
 	}
 }
