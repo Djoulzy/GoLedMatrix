33cd4a79366d8dfbb8b1916186ed6acc8d87dec9
Djoulzy <29226401+Djoulzy@users.noreply.github.com>
2021-12-07 21:03:04 +0100

    confload

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/fogleman/gg v1.3.0
+	github.com/go-ini/ini v1.66.2
 	golang.org/x/exp v0.0.0-20211129234152-8a230f1f7d7a
 	golang.org/x/mobile v0.0.0-20211109191125-d61a72f26a1a
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c
@@ -16,6 +17,7 @@ require (
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/kr/pretty v0.2.1 // indirect
 	github.com/kr/text v0.1.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
 	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
 )
--- main.go
+++ main.go
@@ -6,37 +6,17 @@ import (
 	"image/color"
 	"time"
 
+	"GoLedMatrix/confload"
 	"GoLedMatrix/rgbmatrix"
 
 	"github.com/fogleman/gg"
 )
 
-var (
-	rows                     = flag.Int("led-rows", 32, "number of rows supported")
-	cols                     = flag.Int("led-cols", 64, "number of columns supported")
-	parallel                 = flag.Int("led-parallel", 1, "number of daisy-chained panels")
-	chain                    = flag.Int("led-chain", 4, "number of displays daisy-chained")
-	brightness               = flag.Int("brightness", 100, "brightness (0-100)")
-	hardware_mapping         = flag.String("led-gpio-mapping", "regular", "Name of GPIO mapping used.")
-	show_refresh             = flag.Bool("led-show-refresh", false, "Show refresh rate.")
-	inverse_colors           = flag.Bool("led-inverse", false, "Switch if your matrix has inverse colors on.")
-	disable_hardware_pulsing = flag.Bool("led-no-hardware-pulse", false, "Don't use hardware pin-pulse generation.")
-	slowdown_gpio            = flag.Int("led-slowdown-gpio", 2, "GPIO output speed")
-	pixel_mapper             = flag.String("led-pixel-mapper", "", "Pannel structure")
-)
+var config = &rgbmatrix.HardwareConfig{}
 
 func main() {
-	config := &rgbmatrix.DefaultConfig
-	config.Rows = *rows
-	config.Cols = *cols
-	config.Parallel = *parallel
-	config.ChainLength = *chain
-	config.Brightness = *brightness
-	config.HardwareMapping = *hardware_mapping
-	config.ShowRefreshRate = *show_refresh
-	config.InverseColors = *inverse_colors
-	config.DisableHardwarePulsing = *disable_hardware_pulsing
-	config.PixelMapperConfig = *pixel_mapper
+
+	confload.Load("config.ini", config)
 
 	m, err := rgbmatrix.NewRGBLedMatrix(config)
 	fatal(err)
