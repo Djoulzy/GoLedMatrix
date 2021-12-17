9560b4e4d4da803ea7bd0a4393cff671aaae4a18
Djoulzy <29226401+Djoulzy@users.noreply.github.com>
2021-12-17 08:11:55 +0100

    arm

--- emulator/emulator_arm64.go
+++ emulator/emulator_arm64.go
@@ -1,20 +1,4 @@
 package emulator
 
-import (
-	"image"
-	"image/color"
-	"log"
-	"os"
-	"sync"
-
-	"gioui.org/app"
-	"gioui.org/io/system"
-	"gioui.org/layout"
-	"gioui.org/op"
-	"gioui.org/op/clip"
-	"gioui.org/op/paint"
-	"gioui.org/unit"
-)
-
 type Emulator struct {
 }
