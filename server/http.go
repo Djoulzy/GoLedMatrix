cc224bb8100800a91b487da2156a45c09dc60eb4
Julien Marusi <julien.marusi@bbee.fr>
2021-12-22 19:38:23 +0100

    upload

--- server/http.go
+++ server/http.go
@@ -49,8 +49,11 @@ func (h *HTTP) StartHTTP(config *confload.ConfigData, S *scenario.Scenario) {
 	h.scen = S
 	router := mux.NewRouter()
 	homeVars = templateVars{}
+
 	router.HandleFunc("/", h.homeHandler).Methods("GET")
 	router.HandleFunc("/test", h.testFunc).Methods("POST")
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./server/static"))))
+
 	clog.Output("HTTP Server starting listening on %s", config.HTTPserver.Addr)
 
 	go func() {
